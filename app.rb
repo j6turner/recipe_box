99cc58bad54f87cda1f8a62e67e41d832dab46b6
Carli Martinez & J. TURNER <student@epicodus.com>
2015-01-29 11:05:13 -0800

    Update app.rb, index.erb, and layout.erb files; establish necessary configurations in database.yml file.

--- app.rb
+++ app.rb
@@ -1,1 +1,21 @@
+require("bundler/setup")
+Bundler.require(:default)
+
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @allrecipes = Recipe.all()
+  erb(:index)
+end
+
+post('/recipes') do
+  name = params.fetch('name')
+  @newrecipe = Recipe.create({:name => name})
+  redirect('/')
+end
+
+get("/recipes/:id") do
+  @newrecipe = Recipe.find(params.fetch("id").to_i())
+  @allrecipes = Recipe.all()
+  erb(:recipes)
+end
